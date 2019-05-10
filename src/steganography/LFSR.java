92baed7e3c5a26789fd6f22b0bf0819af0fc33b9
SchmidtSamuelo <SchmidtSamuelo@gmail.com>
2019-05-10 18:09:10 -0400

    LFSR Rewritten
    Rewritten to properly manage a sucessful steganography encryption and decryption

--- src/steganography/LFSR.java
+++ src/steganography/LFSR.java
@@ -4,63 +4,56 @@
  * and open the template in the editor.
  */
 package steganography;
+
 /**
  *
- * @author stRa222Vi2277
+ * @author Randy J Villafuerte
  */
- public final class LFSR {
-    private static final int M = 24;
-    // hard-coded for 32-bits
-    private static final int[] TAPS = {1, 2, 8, 24}; 
-    private final boolean[] bits = new boolean[M + 1];
-    public LFSR(int seed) {
-        for(int i = 0; i < M; i++) {
-            bits[i] = (((1 << i) & seed) >>> i) == 1;
+public class LFSR {
+    //val should be same length as mask in hex.
+    //val is random with above criteria met.
+    int val = 0xA4F;
+    
+    //mask: ignore the one at the end of the feedback polynomial
+    int mask = 0x170;
+    int LFSR = val;
+    int bit;
+    long reset = 0;
+ 
+    public LFSR(){}
+    
+    public int getNextInt(int width, int height)
+    {
+//        System.out.println(LFSR);
+        int size = (width * height) - 1;
+        width = width-1;
+        height = height-1;
+//        System.out.println(size);
+        bit = LFSR & 1;
+        LFSR >>= 1;
+        if (bit == 1)
+            LFSR = LFSR ^ mask;
+//        System.out.println(LFSR);
+//        System.out.println(bit);
+//        LFSR = (LFSR >> 1) | (bit << 15); 
+        if((LFSR > size) || (LFSR < 0))
+        {
+            return getNextInt(width, height);
         }
-    } 
-    public LFSR() {
-        this((int)System.currentTimeMillis());
-    }
-    /* generate a random int uniformly on the interval [-2^31 + 1, 2^31 - 1] */
-    public int nextInt() {
-        printBits(); 
-        // calculate the integer value from the registers
-        int next = 0;
-        for(int i = 0; i < M; i++) {
-            next |= (bits[i] ? 1 : 0) << i;
+        if(LFSR == val)
+        {
+//            System.out.println(0);
+            ResetLFSR();
+            return 0;
         }
-        
-        // allow for zero without allowing for -2^24
-        if (next < 0) next++;
-        
-        // calculate the last register from all the preceding
-        bits[M] = false;
-        for(int i = 0; i < TAPS.length; i++) {
-            bits[M] ^= bits[M - TAPS[i]];
-        }
-        
-        // shift all the registers
-        for(int i = 0; i < M; i++) {
-            bits[i] = bits[i + 1];
-        }
-        return next;
-    } 
-    /** returns random double uniformly over [0, 1) */
-    public double nextDouble() {
-        return ((nextInt() / (Integer.MAX_VALUE + 1.0)) + 1.0) / 2.0;
+           
+        //System.out.println(LFSR);
+        return LFSR;
     }
     
-    /** returns random boolean */
-    public boolean nextBoolean() {
-        return nextInt() >= 0;
+    public void ResetLFSR()
+    {
+        LFSR = val;
+        reset = 0;
     }
-    
-    private void printBits() {
-        System.out.print(bits[M] ? 1 : 0);
-        System.out.print(" -> ");
-        for(int i = M - 1; i >= 0; i--) {
-            System.out.print(bits[i] ? 1 : 0);
-        }
-        System.out.println();
     }
-}
